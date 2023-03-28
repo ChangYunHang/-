b398dd4d48ac9d5b28fb477fd70502b78ecc8f18
ChangYunHang <128287029+ChangYunHang@users.noreply.github.com>
2023-03-28 21:45:32 +0800

    Add files via upload

--- Java-111/src/DAY1/IdentifierTest.java
+++ Java-111/src/DAY1/IdentifierTest.java
@@ -1,6 +1,6 @@
 package DAY1;
 
-;
+
 /*
 * 标识符的使用
 * 1.标识符：凡是自己命名的都叫标识符。比如：类名,方法名，接口名，包名...
@@ -31,9 +31,6 @@ public class IdentifierTest {
 
     }
 
-}
-class  Hello1_$
-{
 }
 
 //class  1Hollo{
@@ -72,7 +69,7 @@ class VariableTest{
 * Java定义的数据类型
 * 一.变量按照数据类型来分
 *    基本数据类型：byte /short/int /long
-*    浮夸型：float/double
+*    浮点型：float/double
 *    字符型：char
 *    布尔型：boolea
 *
@@ -86,8 +83,60 @@ class VariableTest{
 *
 * */
 class VariableTest1{
+
+
     public static void main(String[]args){
         System.out.println("Hello word");
+        //1.整型：byte(1字节=8bit)\shout(2字节)\int(4字节)\long(8字节)
+        //①byte范围：-128~ 127
+        byte b1 = 12;
+        byte b2 = -128;
+        //b2 = 128;//编译不通过
+        System.out.println(b1);
+        System.out.println(b2);
+        //②long型变量必须以"l"或"L"结尾
+        //③通常，定义整型变量时，使用int型
+        short s1 = 12;
+        int i1  = 1234;
+        long l1 = 2132453L;
+        System.out.println(l1);
+
+        //2. 浮点型：float(4字节)/double(8字节)
+        //①浮点型，表示带小数点的数值
+        //②float表示数值的范围比long还大
+
+        double d1 = 123.3;
+        System.out.println(d1+1);
+
+        double f1 = (float) (float) 12.3F;
+        System.out.println(d1+1);
+        //③定义float类型变量时，变量要以"f"或"F"结尾
+        float f2 = 12.3f;
+        System.out.println(f2);
+        //④通常，定义浮点型变量时，使用double型。
+
+        //3.字符型char(1字符=2字节)
+        //①定义char型变量，通常使用一对'',内部只能写一个字符
+        char c1 = 'a';
+        //
+        //c1="AB"
+        System.out.println(c1);
+        char c2 =  '1' ;
+        char c3 = '中';
+        char c4 = 'A';
+
+        //②表示方式：1.声明一个字符 2.转义字符
+        char c5 = '\n';//换行符
+        c5 = '\t';//制表符
+        System.out.println("hellow"+c5);
+        System.out.println("world");
+
+        char c6 = '\u0043';
+        System.out.println(c6);
+
+        //4.布尔型：boolean
+        //①只能取两个值之一给
+
     }
 }
 
