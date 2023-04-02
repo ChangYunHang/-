346a3c72005239f33553a591977ab7b54ba9e484
ChangYunHang <128287029+ChangYunHang@users.noreply.github.com>
2023-04-02 18:41:19 +0800

    Add files via upload

--- Java-111/src/DAY1/IdentifierTest.java
+++ Java-111/src/DAY1/IdentifierTest.java
@@ -255,7 +255,7 @@ class StringTest{
         int number = 1001;
         String numberStr="学号：";
         String info = numberStr+number;//+:连接运算
-        boolean b1=ture;
+        boolean b1=false;
         String info1=info+b1;//+:连接运算
         System.out.println(info);
         //**************
@@ -277,6 +277,31 @@ class StringTest{
         System.out.println('*'+'\t'+"*");//错
         System.out.println('*'+('\t'+"*"));//对
 
+
+    }
+}
+
+/*
+* 计算机中不同进制的使用说明
+*   对于整数有四种表达方式
+* 1.二进制(binary)：0,1,满2进1 ，以0b或者 0B开头
+* 2.十进制(decimal)：0-9，满10进1
+* 3.八进制(octal)：0-7，满8进1，以数字0开头表示
+* 4.十六进制(nex)：0-9及A-F，满16进1，以0x或者0X开头表示。此处A-F不区分大小写。
+* 如：0x21AF=1=0X21B0
+*
+*
+* */
+class BinaryTest{
+    public static void main(String[]args){
+        int num1=0b110;
+        int num2=110;
+        int num3=0127;
+        int num4=0x110A;
+        System.out.println("num1="+num1);
+        System.out.println("num2="+num2);
+        System.out.println("num3="+num3);
+        System.out.println("num4="+num4);
 
     }
 }
