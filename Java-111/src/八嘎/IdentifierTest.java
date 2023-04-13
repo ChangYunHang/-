c7b537e8777def9953070d40bb0061223f962646
ChangYunHang <128287029+ChangYunHang@users.noreply.github.com>
2023-04-14 00:08:14 +0800

    Add files via upload

--- Java-111/src/八嘎/IdentifierTest.java
+++ Java-111/src/八嘎/IdentifierTest.java
@@ -872,4 +872,148 @@ class  IfExer1 {
      }
 }
 
+/*
+*分支结构之二：switch-case
+* 1.格式
+* switch(表达式){
+* case 常量1：
+*    执行语句1；
+*    //break；
+*
+* ......
+*
+* default:
+*  执行语句n;
+*  //break;
+*
+* }
+*
+* 2.说明:
+* ①根据switch表达式中的值，依次匹配各个case中的常量。一旦匹配成功，则进入相应case结构中，调用其执行语句。
+* 当调用完执行语句以后，则仍然继续向下执行其他case结构中的执行语句，直到遇到break关键字或此switch-case结构
+* 末尾结束为止.
+* ②break，可以使用在switch-case结构中，表达一旦执行到此关键字，就跳出switch-case结构
+* ③switch结构中表达式，只能是以下六种数据类型之一：
+* byte、short、char、int、枚举类型、String类型(JDK7.0新增)
+* ④class只能声明常量。不能声明范围。
+* ⑤break关键字是可选的。
+* ⑥default:相当于if-else结构中的else.
+* default结构是可选的，而且位置是灵活的。
+*/
+class SwitchCaseTest{
+    public static void main (String[]args){
+        int number = 2;
+        switch (number){
 
+            case  0:
+                System.out.println("zero");
+                break;
+            case  1:
+                System.out.println("one");
+                break;
+            case  2:
+                System.out.println("two");
+                break;
+            case  3:
+                System.out.println("three");
+                break;
+
+            default:
+                System.out.println("other");
+        }
+
+        //*******************************
+        String season = "summer";
+        switch (season){
+            case "spring":
+            System.out.println("春暖花开");
+            break;
+            case "summer":
+                System.out.println("夏日炎炎");
+                break;
+            case "autumn":
+                System.out.println("秋高气爽");
+                break;
+            case "winter":
+                System.out.println("冬雪皑皑");
+                break;
+            default:
+                System.out.println("季节输入错误");
+                break;
+
+        }
+    }
+
+}
+
+/*
+例题：对学生成绩大于60分的，输出”合格“。低于60分的，输出“不合格”。
+说明：如果switch-case结构中的多个case的执行语句相同，则可以考虑进行合并。
+
+*/
+
+class SwitchCaseTest1 {
+    public static void main(String[] args) {
+        /*
+        int score= 78;
+        switch (score){
+            case 0:
+            case 1:
+                case 2:
+                    ....
+            case 100:
+
+        }
+
+         */
+
+        int score = 78;
+        switch (score / 10) {
+            case 0:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("不及格");
+                break;
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+                System.out.println("及格");
+                break;
+
+
+        }
+    }
+}
+/*
+编写程序：从键盘上输入2019的"month"和"day"，要求通过程序输出的日期为2019的第几天
+2  15：  31  +  15
+5  7 ： 31 +28 + 31 + 30 + 7
+...
+*/
+class SwitchCaseTest2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("请输入2019年的month:");
+        int month = scan.nextInt();
+        System.out.println("请输入2019年的day");
+        int day = scan.nextInt();
+
+        //定义一个变量来保存天数
+        int sumDays = 0;
+        if(month == 1){
+            sumDays = day;
+        }else if(month == 2){
+            sumDays = 31 + day;
+        }else if (month == 3){
+            sumDays = 31 + 28+ day;
+        }else if(month == 4){
+            sumDays = 31 +28 +31 +day ;
+        }
+
+    }
+
+}
